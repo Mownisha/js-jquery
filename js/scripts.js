20982c58ac8878a9e90bb03571d5b8052c37875e
Mownisha <reach.mownisha@gmail.com>
2017-05-01 15:10:00 -0700

    added javascript and jquery to my webpage

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  $(".click").click(function() {
+    $(".js-show").toggle();
+    $(".js-hide").toggle();
+});
+
+  $(".click1").click(function() {
+    $(".jq-show").toggle();
+    $(".jq-hide").toggle();
+  });
+
+  $(".click2").click(function() {
+    $(".ope-show").toggle();
+    $(".ope-hide").toggle();
+});
+
+  $(".click3").click(function() {
+    $(".var-show").toggle();
+    $(".var-hide").toggle();
+  });
+
+  $(".click4").click(function() {
+    $(".vnc-show").toggle();
+    $(".vnc-hide").toggle();
+});
+
+  $(".click5").click(function() {
+    $(".fun-show").toggle();
+    $(".fun-hide").toggle();
+  });
+
+  $(".click6").click(function() {
+    $(".md-show").toggle();
+    $(".md-hide").toggle();
+  });
+
+  $(".click7").click(function() {
+    $(".arg-show").toggle();
+    $(".arg-hide").toggle();
+  });
+
+  $(".click8").click(function() {
+    $(".par-show").toggle();
+    $(".par-hide").toggle();
+});
+
+  $(".click9").click(function() {
+    $(".ret-show").toggle();
+    $(".ret-hide").toggle();
+  });
+
+  $(".click10").click(function() {
+    $(".bool-show").toggle();
+    $(".bool-hide").toggle();
+});
+
+  $(".click11").click(function() {
+    $(".und-show").toggle();
+    $(".und-hide").toggle();
+  });
+});
